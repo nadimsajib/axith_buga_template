dfb22f23c86f34870e32ccd558a0e4be26241dd3
nadimsajib <nhsajib316@gmail.com>
2017-03-28 01:11:05 +0600

    Recent theme published

--- js/script.js
+++ js/script.js
@@ -44,7 +44,35 @@ $(document).ready(function(){
 			"href": this.href.replace(new RegExp("watch\\?v=","i"),'v/'),
 		});
 		return false;
-	})
+	});
+
+	<!-- ============================-->
+	<!-- ===== Portfolio section box ====-->
+	<!-- ============================-->
+
+
+	$('#portfolio a').nivoLightbox({
+		effect: 'fadeScale'
+	});
+
+	<!-- ============================-->
+	<!-- ===== owlcarousel team  ====-->
+	<!-- ============================-->
+
+
+	$('#owl-carousel-team').owlCarousel({
+		/*autoplay: 3000,*/
+		/*items:4,
+		 itemsDesktop: [1199,3],
+		 itemDesktopSmall: [979,3],
+
+		 pagination: true,
+		 paginationNumbers: false,
+
+		 responsive: true,
+		 responsiveRefreshRate: 200,
+		 responsiveBaseWidth: window*/
+	});
 
 });
 
@@ -63,4 +91,67 @@ jQuery(window).scroll(function (){
 	}else{
 		jQuery('.main-menu').removeClass('tiny');
 	}
-})
+});
+
+
+<!-- ================================== -->
+<!-- ========== Map ========= -->
+<!-- ================================== -->
+
+$(document).ready(function() {
+	google.maps.event.addDomListener(window, 'load', init);
+
+	function init() {
+		// Basic options for a simple Google Map
+		// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+		var mapOptions = {
+			// How zoomed in you want the map to start at (always required)
+			zoom: 11,
+
+			// The latitude and longitude to center the map (always required)
+			center: new google.maps.LatLng(40.6700, -73.9400), // New York
+
+			// How you would like to style the map.
+			// This is where you would paste any style found on Snazzy Maps.
+			styles: [{
+				"featureType": "administrative",
+				"elementType": "labels.text.fill",
+				"stylers": [{"color": "#444444"}]
+			}, {
+				"featureType": "landscape",
+				"elementType": "all",
+				"stylers": [{"color": "#f2f2f2"}]
+			}, {"featureType": "poi", "elementType": "all", "stylers": [{"visibility": "off"}]}, {
+				"featureType": "road",
+				"elementType": "all",
+				"stylers": [{"saturation": -100}, {"lightness": 45}]
+			}, {
+				"featureType": "road.highway",
+				"elementType": "all",
+				"stylers": [{"visibility": "simplified"}]
+			}, {
+				"featureType": "road.arterial",
+				"elementType": "labels.icon",
+				"stylers": [{"visibility": "off"}]
+			}, {
+				"featureType": "transit",
+				"elementType": "all",
+				"stylers": [{"visibility": "off"}]
+			}, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#88e9ad"}, {"visibility": "on"}]}]
+		};
+
+		// Get the HTML DOM element that will contain your map
+		// We are using a div with id="map" seen below in the <body>
+		var mapElement = document.getElementById('map');
+
+		// Create the Google Map using our element and options defined above
+		var map = new google.maps.Map(mapElement, mapOptions);
+
+		// Let's also add a marker while we're at it
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(40.6700, -73.9400),
+			map: map,
+			title: 'Snazzy!'
+		});
+	}
+});
